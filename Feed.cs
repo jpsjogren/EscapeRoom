876e8ff72186255e4a669b3c8bffa5cdd6dd4983
Jocke <joachim.sjogren@gmil.com>
2019-11-11 11:53:30 +0100

    * Feed.cs: Bugs..
    * road.cs: Bugs

--- Feed.cs
+++ Feed.cs
@@ -1,6 +1,6 @@
 using System;
 namespace EscapeRoom
-{ 
+{
     public class Feed
     {
 
@@ -9,135 +9,152 @@ namespace EscapeRoom
             inventory = i;
         }
         private Inventory inventory;
-        
+
         public int Room4(int characterType)
-           
-           
+
+
         {
-         
 
-
-            string youdied = @"
-__   __                _ _          _ 
-\ \ / /               | (_)        | |
- \ V /___  _   _    __| |_  ___  __| |
-  \ // _ \| | | |  / _` | |/ _ \/ _` |
-  | | (_) | |_| | | (_| | |  __/ (_| |
-  \_/\___/ \__,_|  \__,_|_|\___|\__,_|
-                                      
-                                      
-";
-
-            string fish = @"     
-      /`·.¸
-     /¸...¸`:·
- ¸.·´  ¸   `·.¸.·´)
-: © ):´;      ¸  {
- `·.¸ `·  ¸.·´\`·¸)
-     `\\´´\¸.·´";
 
             string choice = "";
+            string dead =
+@"
 
-            Console.Clear();
-            Console.WriteLine("Du gick till höger och möter en björn\nVill du skjuta, mata björnen , trädgården");
-            choice = Console.ReadLine().ToLower();
+
+@@@@@                                        @@@@@
+@@@@@@@                                      @@@@@@@
+@@@@@@@           @@@@@@@@@@@@@@@            @@@@@@@
+ @@@@@@@@       @@@@@@@@@@@@@@@@@@@        @@@@@@@@
+     @@@@@     @@@@@@@@@@@@@@@@@@@@@     @@@@@
+       @@@@@  @@@@@@@@@@@@@@@@@@@@@@@  @@@@@
+         @@  @@@@@@@@@@@@@@@@@@@@@@@@@  @@
+            @@@@@@@    @@@@@@    @@@@@@
+            @@@@@@      @@@@      @@@@@
+            @@@@@@      @@@@      @@@@@
+             @@@@@@    @@@@@@    @@@@@
+              @@@@@@@@@@@  @@@@@@@@@@
+               @@@@@@@@@@  @@@@@@@@@
+           @@   @@@@@@@@@@@@@@@@@   @@
+           @@@@  @@@@ @ @ @ @ @@@@  @@@@
+          @@@@@   @@@ @ @ @ @ @@@   @@@@@
+        @@@@@      @@@@@@@@@@@@@      @@@@@
+      @@@@          @@@@@@@@@@@          @@@@
+   @@@@@              @@@@@@@              @@@@@
+  @@@@@@@                                 @@@@@@@
+   @@@@@                                   @@@@@
+
+
+
+__   _______ _   _   _     _____ _____ _____ 
+\ \ / |  _  | | | | | |   |  _  /  ___|  ___|
+ \ V /| | | | | | | | |   | | | \ `--.| |__  
+  \ / | | | | | | | | |   | | | |`--. |  __| 
+  | | \ \_/ | |_| | | |___\ \_/ /\__/ | |___ 
+  \_/  \___/ \___/  \_____/\___/\____/\____/ 
+                                             
+
+";
+
+            string fish = @"                
+                    ,-'.:\
+                 _/'.:'_:'`._    .-._
+            _.-''        ```-`.,'.::.`-._
+         _.'                    ``-..:.:.`-.
+       ,'          ____               `-:.:,'      _..-'|
+     ,',.      ),''    ```--...___        `-.__..'':..  |
+    / (O )   \  `.  ___....-----..``-...___      \::.   |
+   /_' `'  /  )  /,'::::::._:.-'           ````-.-'- .-'|
+   ,-`'. ,' ,'  /  ):::._,'             __...--../::.   |
+   `.        _,'   `--''            _.''           `-.._|
+     `''-..''_                   _.'.:`. 
+              ``.  ..--....___.-' `:_.::/
+                 \ .:`.              `-.\
+                  \:::|
+                   \_,'";
+
+            choice = Console.ReadLine();
+            
 
             do
             {
 
-
                 switch (choice)
                 {
-
                     // mata för att komma till stationen
                     case "mata":
+
+                        if (inventory.hasSalmon)
                         {
-                            if (inventory.hasSalmon)
-                            {
-                              
-                                Console.WriteLine("***************************************\n");
-                                Console.WriteLine(fish);
-                                Console.WriteLine("Du gav björnen en fin Röd Lax och kunde smita förbi till Centralstationen");
-                                Console.WriteLine("Tryck Enter för att fortsätta");
-                                Console.ReadKey();
-
-                                return 5;
-                             }
-
-                            else if (!inventory.hasSalmon)
-                            {
-                                Console.WriteLine("***************************************\n");
-                                Console.WriteLine("Du behöver gå tillbaka till huset och leta mera");
-                                Console.WriteLine("Tryck Enter för att fortsätta");
-                                Console.ReadKey();
-                                Console.Clear();
-                                return 1;
-                            }
-                            break;
-
+                            
+                            Console.WriteLine("***************************************\n");
+                            Console.WriteLine(fish);
+                            Console.WriteLine("Du gav björnen en fin Röd Lax och kunde smita förbi till Centralstationen");
+                            Console.WriteLine("Tryck Enter för att fortsätta");
+                            Console.ReadKey();
+                            Console.Clear();
+                            return 5;
                         }
-
-
-
-
-
-                    //om du använder vapen DÖR du
-                    case "skjuta":
+                        if (!inventory.hasSalmon)
                         {
-                            if (inventory.hasGun)
+                            Console.WriteLine("***************************************\n");
+                            Console.WriteLine("Du behöver gå tillbaka till huset och leta mera");
+                            Console.WriteLine("Tryck Enter för att fortsätta");
+                            Console.ReadKey();
+                            Console.Clear();
+                            return 1;
+                        }
+                        break;
+
+                    //om du skjuter DÖR du
+                    case "skjuta":
+
+                        if (inventory.hasGun)
+                        {
+                           
+                            Console.WriteLine("***************************************\n");
+                            Console.WriteLine("Björnen blev arg och dödade dig");
+                            Console.WriteLine("Tryck Enter för att fortsätta");
+                            Console.ReadKey();
+                            
+                            for (int i = 0; i < 4; i++)
                             {
-                                Console.WriteLine("***************************************\n");
-                                Console.WriteLine("Björnen blev arg och dödade dig");
-                                Console.WriteLine("Tryck Enter för att fortsätta");
-                                Console.ReadKey();
 
-
-                                for (int i = 0; i < 4; i++)
+                                foreach (ConsoleColor c in Enum.GetValues(typeof(ConsoleColor)))
                                 {
 
-                                    foreach (ConsoleColor c in Enum.GetValues(typeof(ConsoleColor)))
-                                    {
-
-                                        Console.ForegroundColor = c;
-                                        Console.WriteLine(youdied);
-                                        System.Threading.Thread.Sleep(100); // 1 sec. deley
-                                        Console.Clear();
-
-                                    }
-
-
-
+                                    Console.ForegroundColor = c;
+                                    Console.WriteLine(dead);
+                                    System.Threading.Thread.Sleep(100); // 1 sec. deley
+                                    Console.Clear();
                                 }
-                                return 1;
                             }
-
-                            // Om du inte har vapen
-                            else if (!inventory.hasGun)
-                            {
-                                Console.WriteLine("***************************************\n");
-                                Console.WriteLine("Har du ett vapen för att skjuta björnen om inte \nHoppas du har en Lax för att locka bort den\n");
-                                Console.WriteLine("Tryck Enter för att fortsätta");
-                                Console.ReadLine();
-
-                                return 2;
-                            }
-                            break;
+                            return 1;
                         }
 
-
-
+                        // Om du inte har vapen
+                        else if (!inventory.hasGun)
+                        {
+                            Console.WriteLine("***************************************\n");
+                            Console.WriteLine("Har du ett vapen för att skjuta björnen om inte \nHoppas du har en Lax för att locka bort den\n");
+                            Console.WriteLine("Tryck Enter för att fortsätta");
+                            Console.ReadKey();
+                            choice = "mata";
+                            return 2;
+                        }
+                        break;
                 }
 
 
+                
                 //fel input  på val av att göra
                 if (choice != "mata" && choice != "skjuta")
                 {
-                    Console.WriteLine("Du använder fel input använd 'mata' skjuta'");
+                    Console.WriteLine("Du använder fel input 'mata ' skjuta");
                     choice = Console.ReadLine();
                     Console.Clear();
                 }
 
-            } while (choice != "mata");
+            } while (choice != "");
             return 1;
 
         }
--- road.cs
+++ road.cs
@@ -13,136 +13,123 @@ namespace EscapeRoom
         public int Room3(int characterType, int fromLevel)
         {
 
-
-
-
             string crossing = @"
    
-___________________________________________________                                               
-                                                 
-                                                 
-                                                 
-_______________                  __________________  
-               |         |       |            
-               |         |       |                
-               |         |       |             
-               |         |       |              
-               |         |       |              
-               |         |       |  ";
+                                /         /  
+                               /         /     
+____________________          /         /
+                    \        /         /    
+                     \      /         /  
+                      \    /         /
+ ____________          \  /         /                
+             \          \/         /                
+              \                   /                
+               \                 /
+               |                |            
+               |                |                
+               |                |             
+               |                |              
+               |                |              
+               |                |  ";
 
 
-            string leavingleft = @"
+            string deadend = @"
+______ _____  ___ ______      _____ _   _______                   
+|  _  \  ___|/ _ \|  _  \    |  ___| \ | |  _  \                  
+| | | | |__ / /_\ \ | | |    | |__ |  \| | | | |                  
+| | | |  __||  _  | | | |    |  __|| . ` | | | |                  
+| |/ /| |___| | | | |/ /     | |___| |\  | |/ /                   
+|___/ \____/\_| |_/___/      \____/\_| \_/___/                    
+ _____ _   _______ _   _      ___  ______ _____ _   _ _   _______ 
+|_   _| | | | ___ \ \ | |    / _ \ | ___ \  _  | | | | \ | |  _  \
+  | | | | | | |_/ /  \| |   / /_\ \| |_/ / | | | | | |  \| | | | |
+  | | | | | |    /| . ` |   |  _  ||    /| | | | | | | . ` | | | |
+  | | | |_| | |\ \| |\  |   | | | || |\ \\ \_/ / |_| | |\  | |/ / 
+  \_/  \___/\_| \_\_| \_/   \_| |_/\_| \_|\___/ \___/\_| \_/___/  ";
 
 
-     _________________
-     |               |
-     |               |
-     |       |       |   
-     |       |       |    
-     |       |       |      
-     |       |       |
-     |       |       |
-";
-
-
-           /* string leavingright = @"
-
-
-      |        |        |   
-      |        |        | 
-      |        |        | 
-      |        |        |
-      |        |        | 
-      |        |        |
-      |        |        |     
-";*/
-
-
-
-
-
-
-
-            // Kommer till korsningen
-            Console.Clear();
-            
+         
+            // Kommer till korsningen 
+            Console.WriteLine("***************************************\n");
             Console.WriteLine(crossing);
             Console.WriteLine("Vill du gå åt höger eller vänster");
             string choice = Console.ReadLine().ToLower();
+            Console.Clear();
 
             do
             {
-
                 switch (choice)
                 {
 
                     // gå åt vänster
                     case "vänster":
-                        {
-                            Console.Clear();
-                            Console.WriteLine("Du gick till vänster och kom till en återvändsgränd");
-                            Console.WriteLine(leavingleft);
-                            Console.WriteLine("Gå tillbaka till korsningen?");
-                            Console.WriteLine("Tryck enter för att fortsätta");
-                            choice = "korsningen";
-                            Console.ReadKey();
-                          
 
+                        Console.WriteLine("***************************************\n");
+                        Console.WriteLine("Du gick till vänster");
+                        Console.WriteLine(deadend);
+                        Console.WriteLine("***************************************\n");
+                        Console.WriteLine("Gå tillbaka till korsningen?");
+                        Console.WriteLine("tryck Enter för att fortsätta");
+                        Console.ReadKey();
+                        Console.Clear();
 
-                        }
                         break;
 
 
                     // Tillbaka till korningen
                     case "korsningen":
-                        {
 
-                            Console.Clear();
-                            Console.WriteLine(crossing);
-                            Console.WriteLine("Du gick tillbaka till korsningen");
-                            Console.WriteLine("Du vill nu gå ut höger");
-                            Console.WriteLine("Skriv höger");
-                            Console.ReadLine();
-                            Console.Clear();
-                            
-
-                        }
+                        Console.WriteLine("***************************************\n");
+                        Console.WriteLine(crossing);
+                        Console.WriteLine("Du gick tillbaka till korsningen");
+                        Console.WriteLine("Du vill nu gå ut höger");
+                        Console.WriteLine("Skriv höger");
+                        Console.ReadLine();
+                        Console.Clear();
                         break;
-                        
 
                     // gå till höger
                     case "höger":
-                        {
-                            return 4;
-                        }
-                       
+
+                        Console.WriteLine("***************************************\n");
+                        Console.WriteLine("Du gick till höger och möter en björn\nVill du skjuta, mata");
+                   
+                        
+                        return 4;
+
 
                     // Tillbaka till trädgården
-                    case "trädgården":
+                    case "tillbaka":
+                        Console.WriteLine("***************************************\n");
+                        if (!inventory.hasGun || !inventory.hasSalmon || !inventory.hasMap || !inventory.hasWallet || !inventory.hasKey)
 
-                        if (!inventory.hasGun && !inventory.hasSalmon || !inventory.hasMap || !inventory.hasWallet || !inventory.hasKey)
-                        {
-                            Console.WriteLine("Du behöver gå tillbaka till trädgården\noch leta mera\n");
-                            Console.WriteLine("Tryck Enter för att fortsätta");
-                            Console.ReadKey();
-                            Console.Clear();
-                        }
-                        return 2;
-                    default:
-                        Console.WriteLine("Du använder fel input, använd ' vänster ' höger 'korsningen ");
-                        choice = Console.ReadLine();
+                        Console.WriteLine("Du behöver gå tillbaka till trädgården\noch leta mera\n");
+                        Console.WriteLine("Tryck Enter för att fortsätta");
+                        Console.ReadKey();
                         Console.Clear();
-                        break;
+                        return 2;
+
                 }
 
 
-            } while (choice !="höger");
 
-            return 4;
+                if (choice != "vänster" && choice != "höger" && choice != "tillbaka" && choice != "korsningen")
+                {
+                    Console.WriteLine("Du använder fel input. tryck Enter för att gå tillbaka. ");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                break;
+
+
+
+            } while (choice != "mata" && choice != "skjuta");
+            return 1;
+
         }
     }
 }
 
 
 
-          
+
